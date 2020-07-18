14cc5f3e5e25bc7a69820af03daaf4dfa550bce9
Maysoon Tabali <mtab03@me.com>
2020-07-18 20:25:34 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,20 @@
 # Write your methods here
+def loop_message_five_times(string)
+  5.times {puts "Hello World."}
+  end 
+#10.times { puts "hello" }
+
+def loop_message_n_times(message, number)
+  5.times {puts "Hello Moon."}
+  10.times {puts "Hello Red Balloon."}
+end 
+
+def output_array(array)
+ count = 0 
+ while count < array.length 
+ do 
+   puts array [count]
+   count + = 1 
+ end 
+end 
+
